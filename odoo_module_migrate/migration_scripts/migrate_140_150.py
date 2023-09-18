0e68e662d9d9d22035896f7c99787e4175e7bfb2
emilkholod <kholodnov@arrival.com>
2023-09-18 10:32:45 +0500

    [ADD] Information about migration from 14 to 15

--- odoo_module_migrate/migration_scripts/migrate_140_150.py
+++ odoo_module_migrate/migration_scripts/migrate_140_150.py
@@ -4,4 +4,11 @@ from odoo_module_migrate.base_migration_script import BaseMigrationScript
 
 
 class MigrationScript(BaseMigrationScript):
+    """
+    Common information can be found here: https://github.com/OCA/maintainer-tools/wiki/Migration-to-version-15.0
+    Javascript migration to owl:
+    - https://www.odoo.com/documentation/15.0/developer/reference/frontend/framework_overview.html
+    - https://codingdodo.com/odoo-15-owl-view-migration-guide/
+    """
+
     pass
