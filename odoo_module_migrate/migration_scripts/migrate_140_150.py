1acdcfa9a2646b0fec5c07d20858efd682f002a3
emilkholod <kholodnov@arrival.com>
2023-09-25 16:12:03 +0500

    [IMP] Add warnings about x2many operations

--- odoo_module_migrate/migration_scripts/migrate_140_150.py
+++ odoo_module_migrate/migration_scripts/migrate_140_150.py
@@ -2,7 +2,12 @@
 
 from odoo_module_migrate.base_migration_script import BaseMigrationScript
 
-
+x2many_operations = {
+    r"[\(\[][ \n]*[0-1],[ \n]*[^,]*,[ \n]*{": "[15] It seems there is an old style x2many operations in file (starting from 0 or 1). Please use Command to manipulate with x2many items.",
+    r"[\(\[][ \n]*[2-4],": "[15] It seems there is an old style x2many operations in file (starting from 2, 3 or 4). Please use Command to manipulate with x2many items.",
+    r"[\(\[][ \n]*5,": "[15] It seems there is an old style x2many operations in file (starting from 5). Please use Command to manipulate with x2many items.",
+    r"[\(\[][ \n]*6,[ \n]*[^,]*,[ \n]": "[15] It seems there is an old style x2many operations in file (starting from 6). Please use Command to manipulate with x2many items.",
+}
 class MigrationScript(BaseMigrationScript):
     """
     Common information can be found here: https://github.com/OCA/maintainer-tools/wiki/Migration-to-version-15.0
@@ -11,4 +16,11 @@ class MigrationScript(BaseMigrationScript):
     - https://codingdodo.com/odoo-15-owl-view-migration-guide/
     """
 
-    pass
+    _TEXT_WARNINGS = {
+        ".py": {
+            **x2many_operations,
+        },
+        ".xml": {
+            **x2many_operations,
+        },
+    }
